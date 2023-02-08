dbfb7a6f462cffedc4ed7df09828de1c78a37584
anejolov <118720241+anejolov@users.noreply.github.com>
2023-02-08 13:07:39 +0200

    Add files via upload

--- Windeckhelper.ps1
+++ Windeckhelper.ps1
@@ -638,12 +638,12 @@ function run_form
 
             ## AveYo: fix ms-gamebar annoyance after uninstalling Xbox
             reg add HKCR\ms-gamebar /f /ve /d URL:ms-gamebar 2>&1 >''
-            reg add HKCR\ms-gamebar /f /v "URL Protocol" /d "" 2>&1 >''
-            reg add HKCR\ms-gamebar /f /v "NoOpenWith" /d "" 2>&1 >''
+            reg add HKCR\ms-gamebar /f /v "URL Protocol" /d " " 2>&1 >''
+            reg add HKCR\ms-gamebar /f /v "NoOpenWith" /d " " 2>&1 >''
             reg add HKCR\ms-gamebar\shell\open\command /f /ve /d "\`"$env:SystemRoot\System32\systray.exe\`"" 2>&1 >''
             reg add HKCR\ms-gamebarservices /f /ve /d URL:ms-gamebarservices 2>&1 >''
-            reg add HKCR\ms-gamebarservices /f /v "URL Protocol" /d "" 2>&1 >''
-            reg add HKCR\ms-gamebarservices /f /v "NoOpenWith" /d "" 2>&1 >''
+            reg add HKCR\ms-gamebarservices /f /v "URL Protocol" /d " " 2>&1 >''
+            reg add HKCR\ms-gamebarservices /f /v "NoOpenWith" /d " " 2>&1 >''
             reg add HKCR\ms-gamebarservices\shell\open\command /f /ve /d "\`"$env:SystemRoot\System32\systray.exe\`"" 2>&1 >''
 
             Write-Host -ForegroundColor Green "Done"
